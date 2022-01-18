fd1511d6dd92b7cc19d01c33156dc6a12ede82c6
ankith88 <ankith88@gmail.com>
2022-01-19 10:26:14 +1100

    CU-1unh28y
    Schedule script to call RTA API and inform about deactivating customer

---
+++ mp_ss_change_status_lost.js
@@ -0,0 +1,61 @@
+/**
+ * Module Description
+ *
+ * NSVersion    Date                        Author
+ * 1.00         2021-11-15 13:18:23         Ankith
+ *
+ * Description: Hit RTA API to inform to deactivate customer.
+ *
+ * @Last modified by:   ankithravindran
+ * @Last modified time: 2022-01-19T10:00:35+11:00
+ *
+ */
+
+var tollPODPDF = 324;
+
+var adhoc_inv_deploy = 'customdeploy2';
+var prev_inv_deploy = null;
+var ctx = nlapiGetContext();
+var usageThreshold = 50;
+
+function main() {
+
+  //SEARCH: Customer - Status Change - Lost
+  var customerLostTomorrowSearch = nlapiLoadSearch('customer',
+    'customsearch_customer_change_status_lost');
+
+  var resultCustomerLostTomorrowSearch = customerLostTomorrowSearch.runSearch();
+
+  resultCustomerLostTomorrowSearch.forEachResult(function(searchResult) {
+    var internalID = searchResult.getValue("internalid");
+    var entityID = searchResult.getValue("entityid");
+    var companyName = searchResult.getValue("compnayname");
+    var partnerID = searchResult.getValue("partner");
+    var partnerText = searchResult.getText("partner");
+
+    nlapiSendEmail(696992, ['mailplussupport@protechly.com'],
+      'Deactivate Customer', ' Customer NS ID: ' + internalID +
+      '</br> Customer: ' + entityID + ' ' + companyName +
+      '</br> Customer Franchisee NS ID: ' + partnerID, [
+        'raine.giderson@mailplus.com.au',
+        'ankith.ravindran@mailplus.com.au',
+        'rianne.mansell@mailplus.com.au',
+        'fiona.harrison@mailplus.com.au'
+      ]);
+
+    var customerJSON = '{';
+    customerJSON += '"ns_id" : "' + internalID + '"'
+    customerJSON += '}';
+
+    var headers = {};
+    headers['Content-Type'] = 'application/json';
+    headers['Accept'] = 'application/json';
+    headers['x-api-key'] = 'XAZkNK8dVs463EtP7WXWhcUQ0z8Xce47XklzpcBj';
+
+    nlapiRequestURL('https://mpns.protechly.com/deactivate_customer',
+      customerJSON,
+      headers);
+    return true;
+  });
+
+}
